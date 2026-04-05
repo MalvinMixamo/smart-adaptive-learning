0dbcb88757e72d505767370716b3e2bd6ec37ea5
Kipin.Kipon <malvinmixamo@gmail.com>
2026-04-05 18:24:46 +0700

    done perkara meeting

--- app/api/meeting/lock/route.ts
+++ app/api/meeting/lock/route.ts
@@ -1,6 +1,14 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
-import pusher from "pusher";
+import PusherServer from "pusher"; // Kasih nama alias 'PusherServer'
+
+const pusher = new PusherServer({
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
+  secret: process.env.PUSHER_SECRET!,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+  useTLS: true,
+});
 
 export async function POST(req: Request) {
   const { roomId, status } = await req.json();
